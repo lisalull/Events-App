8c2beec09202c8ddd2d43a075c00fc31a864c7f1
lisa <lisa.marie.lull@gmail.com>
2021-11-08 10:43:47 -0500

    added components and routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,34 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./components/Homepage";
+import SearchForm from "./components/SearchForm";
+import Details from "./components/Details";
+import BucketList from "./components/BucketList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Homepage />
+          </Route>
+          <Route path="/events/:id/details" exact>
+            <Details />
+          </Route>
+          <Route path="/events/bucketlist">
+            <BucketList />
+          </Route>
+          <Route path="/">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
